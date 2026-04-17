0458e11ad0b8cddd90b655db809e4ecf98b479ad
dev-caiodias <dev.caiodias19@gmail.com>
2026-04-17 20:40:12 -0300

    esqueleto exercicio4

--- src/Dependente.java
+++ src/Dependente.java
@@ -4,7 +4,7 @@ public class Dependente extends Pessoa{
 
     protected Parentesco parentesco;
 
-     public Dependente(Date dataNascimento, String cpf, String nome) {
+    public Dependente(Date dataNascimento, String cpf, String nome) {
         super(dataNascimento, cpf, nome);
     }
 
