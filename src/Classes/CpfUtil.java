9338f8f18b9fc3e10a658f5f60f81b217acdb5ad
dev-caiodias <dev.caiodias19@gmail.com>
2026-04-20 21:33:48 -0300

    valida cpf duplo

--- src/Classes/CpfUtil.java
+++ src/Classes/CpfUtil.java
@@ -7,5 +7,10 @@ public class CpfUtil {
 
     protected static Set<String> cpfs = new HashSet<>();
 
-
+    public static void verificarDuplicado(String cpf){
+        if (cpfs.contains(cpf)){
+            throw new IllegalArgumentException(("CPF já cadastrado no sistema."));
+        }
+        cpfs.add(cpf);
+    }
 }
