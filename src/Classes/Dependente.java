31f85227750779fe397f10eac577c47a134d5c81
Rafael Albino <ansdles@live.com>
2026-04-22 20:53:17 -0300

    feat: melhoria na estrutura e inserção de comandos gets

--- src/Classes/Dependente.java
+++ src/Classes/Dependente.java
@@ -1,18 +1,27 @@
 package Classes;
 
+import Exceptions.DependenteException;
+
 import java.time.LocalDate;
 import java.time.Period;
 
 public class Dependente extends Pessoa {
-    protected Parentesco parentesco;
+    private Parentesco parentesco;
+    private int id;
 
-    public Dependente(LocalDate dataNascimento, String cpf, String nome, Parentesco parentesco) {
+    public Dependente(LocalDate dataNascimento, String cpf, String nome, Parentesco parentesco)
+            throws DependenteException {
         super(dataNascimento, cpf, nome);
+        this.parentesco = parentesco;
+
+        // Exception para idade do dependente.
         int idade = Period.between(dataNascimento, LocalDate.now()).getYears();
         if (idade >= 18){
             throw new DependenteException("O dependente deve ter menos de 18 anos.");
         }
-
-        this.parentesco = parentesco;
     }
+
+    // Getters ↓
+    public int getId(){ return id; }
+    public Parentesco getParentesco() { return parentesco; }
 }
