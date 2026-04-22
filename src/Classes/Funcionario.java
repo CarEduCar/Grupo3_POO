611660e6aa6ffcdbde0f50e5d4a0f2124d9523ad
Rafael Albino <ansdles@live.com>
2026-04-22 19:27:10 -0300

    feat: adicionado validações de segurança

--- src/Classes/Funcionario.java
+++ src/Classes/Funcionario.java
@@ -3,6 +3,7 @@ package Classes;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.time.Period;
 
 public class Funcionario extends Pessoa {
     protected double salarioBruto;
@@ -14,6 +15,19 @@ public class Funcionario extends Pessoa {
         super(dataNascimento, cpf, nome);
         this.salarioBruto = salarioBruto;
         dependente = new ArrayList<>();
+
+        //Validações de segurança.
+        if(this.salarioBruto <= 0.0) throw new IllegalArgumentException("Salário não pode ser <= que 0!");
+        if(nome.isEmpty() || nome.isBlank()) throw new IllegalArgumentException("Nome está vazio!");
+        if(cpf.isEmpty() || cpf.isBlank()) throw new IllegalArgumentException("CPF está vazio!");
+        if(cpf.length() != 11) throw new IllegalArgumentException("CPF inválido ou menor/maior que 11 Digitos!");
+
+        // ↓ Aqui o '\\d' significa que aceita de 0-9 e o '+' que aceita um ou mais de um número ↓
+        if(!cpf.matches("\\d+")) throw new IllegalArgumentException("CPF deve conter apenas números!");
+        int idade = Period.between(dataNascimento, LocalDate.now()).getYears();
+        if(idade >= 0 && idade < 18) throw new IllegalArgumentException("Data de nascimento (Menor de Idade)!");
+        if(idade < 0 || idade > 120) throw new IllegalArgumentException
+                ("Data de nascimento inválida (Fora da faixa válida 'MENOR que 0 ou MAIOR que 120 anos'!");
     }
 
     public double calcularINSS(double salario) {
