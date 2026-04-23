e19eb99ba4af366632be7969afde0cd8b857509d
Rafael Albino <ansdles@live.com>
2026-04-23 11:49:55 -0300

    feat: método outputFile criado e chamada na Main

--- src/File/OutputFile.java
+++ src/File/OutputFile.java
@@ -1,8 +1,41 @@
 package File;
+
+import Classes.FolhaPagamento;
+import Classes.Funcionario;
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.Locale;
+
 public class OutputFile {
 
     //Construtor ↓
     public OutputFile(){
+    }
 
+    //Metodo que recebe a folha de pagamento e um nome para o arquivo de saída CSV
+    public void processarFolha(FolhaPagamento folha, String nomeArquivo){
+        Locale padraoBR = new Locale("pt", "BR"); //Aqui garante o uso da virgula.
+        try(PrintWriter writer = new PrintWriter(new FileWriter(nomeArquivo))){
+            //Cria uma instancia de Funcionario.
+            Funcionario funcionario = folha.getFuncionario();
+
+            folha.getSalarioLiquido(); // Aqui a função é chamada para alimentar as variáveis DescontoINSS e DescontoIR previamente.
+
+            //Monta toda a estrutura da String.
+            String linha = String.format("%s;%s;%f;%f;%f",
+                    funcionario.getNome(),
+                    funcionario.getCpf(),
+                    folha.getDescontoINSS(),
+                    folha.getDescontoIR(),
+                    folha.getSalarioLiquido());
+
+            writer.println(linha); //Imprimi no arquivo
+            System.out.println("Arquivo CSV criado com Sucesso!");
+
+        } catch (Exception exception) {
+            System.err.println("Erro ao processar arquivo CSV, talvez seja problema de permissão de escrita no disco"
+                    + exception.getMessage());
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import Classes.FolhaPagamento;
+import Classes.Funcionario;
+import File.OutputFile;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+        //Cria um funcionário (Chamada de teste)
+        Funcionario funcionario1 = new Funcionario(LocalDate.of(1990,5,5), "12345678911",
+                "Rafael", 5000.0);
+
+        //Cria uma folha
+        FolhaPagamento folhaPagamento = new FolhaPagamento(funcionario1, LocalDate.now());
+
+        //Cria o Arquivo de Saída.
+        OutputFile outputFile = new OutputFile();
+        outputFile.processarFolha(folhaPagamento,"ArquivoSaída.CSV");
 
     }
 }
