3de7334d0881202248d675158ad63b85d7a69488
CarEduCar <carloscarvalho201532@gmail.com>
2026-04-23 17:40:03 -0300

    correção de erros e possiveis bugs

--- src/File/InputFile.java
+++ src/File/InputFile.java
@@ -3,19 +3,23 @@ package File;
 import Classes.Dependente;
 import Classes.Funcionario;
 import Classes.Parentesco;
+import Exceptions.DependenteException;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InputFile{
 
-    public void leitorCSV (String caminho) throws IOException{
-        BufferedReader br = new BufferedReader(new FileReader(caminho);
+    public void leitorCSV (String caminho) throws IOException, DependenteException {
+        BufferedReader br = new BufferedReader(new FileReader(caminho));
         String linha;
         DateTimeFormatter formatarData = DateTimeFormatter.ofPattern("yyyyMMdd");
+        List<Funcionario> listaFuncionarios = new ArrayList<>();
 
         while((linha = br.readLine()) != null){
             String[] particoesFuncionario = linha.split(";");
@@ -25,19 +29,25 @@ public class InputFile{
             LocalDate data_nascF = LocalDate.parse(dataStrF, formatarData);
             Double salario = Double.parseDouble(particoesFuncionario[3]);
 
-            Funcionario funcionario= new Funcionario(data_nascF, cpfF,nomeF, salario);
-            while(!((linha = br.readLine()).isEmpty())){
+            Funcionario funcionario = new Funcionario(data_nascF, cpfF,nomeF, salario);
+            listaFuncionarios.add(funcionario);
+
+            while(true){
+                linha = br.readLine();
+                if (linha == null || linha.trim().isEmpty() ){
+                    break;
+                }
+
                 String[] particoesDependente = linha.split(";");
-                String nomeD = particoesFuncionario[0];
-                String cpfD = particoesFuncionario[1];
-                String dataStrD = particoesFuncionario[2];
+                String nomeD = particoesDependente[0];
+                String cpfD = particoesDependente[1];
+                String dataStrD = particoesDependente[2];
                 LocalDate data_nascD = LocalDate.parse(dataStrD, formatarData);
-                String parente = particoesDependente[3].toUpperCase();
+                String parente = particoesDependente[3].trim().toUpperCase();
                 Parentesco parentesco = Parentesco.valueOf(parente);
 
                 Dependente dependente= new Dependente(data_nascD, cpfD, nomeD, parentesco);
             }
-
         }
     }
 }
