4327924b17bd1236d0ef84c50e201d60fb9f2dee
Rafael Albino <ansdles@live.com>
2026-04-22 21:07:16 -0300

    feat: melhorias

--- src/Classes/Funcionario.java
+++ src/Classes/Funcionario.java
@@ -6,17 +6,18 @@ import java.util.List;
 import java.time.Period;
 
 public class Funcionario extends Pessoa {
-    protected double salarioBruto;
-    protected double descontoInss;
-    protected double descontoIR;
-    protected List<Dependente> dependente;
+    private double salarioBruto;
+    //private double descontoInss;
+    //private double descontoIR;
+    private List<Dependente> dependente = new ArrayList<>();
 
+    // Construtor ↓
     public Funcionario(LocalDate dataNascimento, String cpf, String nome, double salarioBruto) {
         super(dataNascimento, cpf, nome);
         this.salarioBruto = salarioBruto;
         dependente = new ArrayList<>();
 
-        //Validações de segurança.
+        //Validações de segurança ↓
         if(this.salarioBruto <= 0.0) throw new IllegalArgumentException("Salário não pode ser <= que 0!");
         if(nome.isEmpty() || nome.isBlank()) throw new IllegalArgumentException("Nome está vazio!");
         if(cpf.isEmpty() || cpf.isBlank()) throw new IllegalArgumentException("CPF está vazio!");
@@ -30,40 +31,8 @@ public class Funcionario extends Pessoa {
                 ("Data de nascimento inválida (Fora da faixa válida 'MENOR que 0 ou MAIOR que 120 anos'!");
     }
 
-    public double calcularINSS(double salario) {
-        if (salario <= 1518.00) {
-            return salario * 0.075;
-        } else if (salario <= 2793.88) {
-            return salario * 0.09 - 22.77;
-        } else if (salario <= 4190.83) {
-            return salario * 0.12 - 106.60;
-        } else if (salario <= 8157.41) {
-            return salario * 0.14 - 190.42;
-        } else {
-            return 951.62;
-        }
-    }
+    // Getters ↓
+    public double getSalarioBruto(){ return this.salarioBruto; }
+    public List<Dependente> getDependente(){ return this.dependente; }
 
-    public double calcularIR(double salarioBruto, int dependentes) {
-        double inss = calcularINSS(salarioBruto);
-        double base = salarioBruto - inss - (dependentes * 189.59);
-
-        if (base <= 2259.20) {
-            return 0;
-        } else if (base <= 2826.65) {
-            return base * 0.075 - 169.44;
-        } else if (base <= 3751.05) {
-            return base * 0.15 - 381.44;
-        } else if (base <= 4664.68) {
-            return base * 0.225 - 662.77;
-        } else {
-            return base * 0.275 - 896.00;
-        }
-    }
-
-    public double calcularSalarioLiquido(double salarioBruto, int dependentes) {
-        double inss = calcularINSS(salarioBruto);
-        double ir = calcularIR(salarioBruto, dependentes);
-        return salarioBruto - inss - ir;
-    }
 }
